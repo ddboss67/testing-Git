04d330ac58de6f6acbe213267c3c29089f8fe088
Damian Kryśko <trucker.dragon02@gmail.com>
2019-11-15 14:36:47 +0100

    add function add

--- Program.cs
+++ Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace testing_Git
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int a, b;
+            Console.WriteLine("Podaj A: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj B: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dodawanie:" + Add(a, b));
+            Console.ReadKey();
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
